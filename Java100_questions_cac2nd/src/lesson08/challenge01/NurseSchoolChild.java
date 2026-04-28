3416b142a0f29a7156d8cceb58b80d5098cec25c
sminemu <sminemu@C010423630.zipangu.cac.co.jp>
2026-04-28 17:30:50 +0900

    4/28 lesson08-lesson09.challenge03まで完了

---
+++ Java100_questions_cac2nd/src/lesson08/challenge01/NurseSchoolChild.java
@@ -0,0 +1,23 @@
+package lesson08.challenge01;
+
+public class NurseSchoolChild {
+
+	private String name;
+
+	public NurseSchoolChild(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	void Play() {
+		System.out.println("遊ぶよ～。");
+	}
+
+}
